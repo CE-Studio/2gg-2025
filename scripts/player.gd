031bd0b99f5df29873e68005f300cdd0b617647d
clarence112 <clarencepacelli@gmail.com>
2025-05-02 20:52:04 -0400

    a

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,25 +2,84 @@ class_name Player
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var SPEED = 600.0
+var JUMP_VELOCITY = -810.0
+var ACCEL = 6000.0
+
+
+enum Masks {
+	NONE,
+	FOX,
+	GECKO,
+	BEAR,
+	CAT,
+	BEETLE,
+}
+
+
+signal state_changed
+
+
+static var has_mask:Array[bool] = [
+	true,
+	false,
+	false,
+	false,
+	false,
+	false,
+]
+
+
+static var instance:Player
+
+
+@export var mask_state:Masks = Masks.NONE:
+	set(value):
+		mask_state = value
+		_sync_motion()
+		state_changed.emit()
+
+
+func _ready() -> void:
+	instance = self
+	state_changed.emit()
+
+
+func _sync_motion() -> void:
+	match mask_state:
+		Masks.NONE:
+			JUMP_VELOCITY = -800.0
+			up_direction = Vector2.UP
+		Masks.FOX:
+			JUMP_VELOCITY = -800.0
+			up_direction = Vector2.UP
+		Masks.GECKO:
+			pass
+		Masks.BEAR:
+			JUMP_VELOCITY = -800.0
+			up_direction = Vector2.UP
+		Masks.CAT:
+			JUMP_VELOCITY = -800.0
+			up_direction = Vector2.UP
+		Masks.BEETLE:
+			JUMP_VELOCITY = -800.0
+			up_direction = Vector2.UP
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+
+	if Input.is_action_just_pressed("game_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+
+	var direction := Input.get_axis("game_left", "game_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCEL * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, ACCEL * delta)
+
 
 	move_and_slide()
