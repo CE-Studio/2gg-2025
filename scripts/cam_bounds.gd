e9e597d604d9c6369614895b10dcbbafef39ee56
EpsilonTheDerg <jamesclamey@gmail.com>
2025-05-05 21:17:20 -0400

    Moar tiles, camera bounds

---
+++ scripts/cam_bounds.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+@onready var box:CollisionShape2D = $"CollisionShape2D"
+
+
+func _ready() -> void:
+	$"/root/GlobalCamera".bounds = self
+
+
+func get_coord_bounds() -> Array:
+	var output = []
+	var half_size = Vector2(box.shape.size.x, box.shape.size.y) * 0.5
+	output.append(box.position.x - half_size.x)
+	output.append(box.position.x + half_size.x)
+	output.append(box.position.y - half_size.y)
+	output.append(box.position.y + half_size.y)
+	return output
