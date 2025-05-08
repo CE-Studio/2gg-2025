8807ab56664ef8a16386e0b733d2af6021a9495b
clarence112 <clarencepacelli@gmail.com>
2025-05-07 22:26:42 -0400

    a

--- scripts/throwable.gd
+++ scripts/throwable.gd
@@ -7,7 +7,13 @@ extends RigidBody2D
 
 func _init() -> void:
 	body_entered.connect(_on_body_entered)
+	can_sleep = false
 
 
 func  _on_body_entered(body:Node) -> void:
 	pass
+
+
+func _physics_process(delta: float) -> void:
+	if is_instance_valid(Player.instance):
+		gravity_scale = Player.instance.obj_grav_dir
